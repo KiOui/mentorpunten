e6ef51c2006f28a4dd76d0097000d6c64a5321ab
Lars van Rhijn <larsvanrhijn@gmail.com>
2023-07-04 21:42:17 +0200

    Fix flake

--- website/transactions/api/v1/views.py
+++ website/transactions/api/v1/views.py
@@ -1,6 +1,4 @@
-from rest_framework import status
 from rest_framework.generics import CreateAPIView
-from rest_framework.response import Response
 
 from mentorpunten.api.openapi import CustomAutoSchema
 from transactions.api.v1.serializers import AccountSerializer
@@ -25,20 +23,3 @@ class AccountRetrieveAPIView(CreateAPIView):
 
     serializer_class = AccountSerializer
     queryset = Account.objects.all()
-
-    def post(self, request, **kwargs):
-        """POST request."""
-        token = request.data.get("token", None)
-        if token is None:
-            return Response(status=status.HTTP_400_BAD_REQUEST)
-
-        valid, signed_material = verify_identification_token(token, 900000)
-        if not valid:
-            return Response(status=status.HTTP_400_BAD_REQUEST)
-        try:
-            account = self.queryset.get(user__username=signed_material)
-            return Response(
-                status=status.HTTP_200_OK, data=self.serializer_class(account).data
-            )
-        except Account.DoesNotExist:
-            return Response(status=status.HTTP_404_NOT_FOUND)
