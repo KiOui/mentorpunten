61f07b9f4674370e0954a5b35b91148b1261d015
nvoers <nick-vanoers@hotmail.com>
2023-07-07 12:50:07 +0200

    challenge card

--- frontend/src/components/ChallengeCard.vue
+++ frontend/src/components/ChallengeCard.vue
@@ -1,10 +1,16 @@
+<script lang="ts">
+import router from '@/router';
+
+</script>
+
 <template>
     <div class="card my-2">
+        <router-link :to="{ name: 'challenge', params: { id: 1 } }">
         <div class="card-body">
-            <h2 class="card-title">Challenge name</h2>
-            <a class="btn btn-primary m-1">View</a>
-            <a class="btn btn-primary m-1">Submit</a>
+            <h3 class="card-title">Challenge name</h3>
+            <p class="card-text">Challenge description</p>
         </div>
+        </router-link>
     </div>
 </template>
 
