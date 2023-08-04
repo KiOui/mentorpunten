df059a0e285f8bf274a4353572956728a3c4c3fc
Lars van Rhijn <larsvanrhijn@gmail.com>
2023-08-04 21:47:35 +0200

    Start of statistics

---
+++ frontend/src/components/TournamentStatisticsCard.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+    import { RouterLink } from 'vue-router'
+    import type Tournament from "@/models/tournament.model";
+
+    defineProps<{tournament: Tournament}>();
+
+    function start_end_time(tournament: Tournament): string {
+      if (tournament.active_from) {
+        const start_date = new Date(tournament.active_from);
+        const end_date = new Date(tournament.active_until);
+        const today_date = new Date();
+
+        return `${start_date.toLocaleDateString("en-GB")}, ${start_date.toLocaleTimeString(
+            "en-GB",
+          { hour: "2-digit", minute: "2-digit" }
+        )} until ${end_date.toLocaleDateString("en-GB")}, ${end_date.toLocaleTimeString(
+            "en-GB",
+          { hour: "2-digit", minute: "2-digit" }
+        )}`;
+      }
+      return "";
+    }
+    
+</script>
+
+<template>
+    <router-link :to="{ name: 'Challenges', params: { id: tournament.id }}" style="text-decoration: none;">
+        <div class="custom-card">
+            <h1>{{ tournament.name }}</h1>
+            <h3>{{ start_end_time(tournament) }}</h3>
+        </div>
+    </router-link>
+</template>
+
+<style scoped>
+</style>
--- frontend/src/views/StatisticsView.vue
+++ frontend/src/views/StatisticsView.vue
@@ -1,34 +1,42 @@
 <script setup lang="ts">
-  import { useCredentialsStore } from '@/stores/credentials.module';
   import useApiService from "@/common/api.service";
-  import {computed, type ComputedRef, onMounted, ref, toRaw} from 'vue';
-  import type Team from "@/models/team.model";
+  import {onMounted, ref} from "vue";
+  import type Tournament from "@/models/tournament.model";
   import Loader from "@/components/Loader.vue";
-  import Header from "@/components/Header.vue";
+  import TournamentStatisticsCard from "@/components/TournamentStatisticsCard.vue";
+  import type Team from "@/models/team.model";
 
-  const store = useCredentialsStore();
   const ApiService = useApiService();
+
+  const tournaments = ref<Tournament[] | null>(null);
+  const tournamentsLoading = ref<boolean | null>(true);
+
   const teams = ref<Team[] | null>(null);
-  const teamsLoading = ref<boolean>(true);
+  const teamsLoading = ref<boolean | null>(true);
 
   onMounted(() => {
+    ApiService.getTournaments().then(result => {
+      tournaments.value = result;
+      tournamentsLoading.value = false;
+    }).catch(() => {
+      tournamentsLoading.value = null;
+    });
+
     ApiService.getChallengesTeams().then(result => {
       teams.value = result;
       teamsLoading.value = false;
-    });
-  });
-
-  const sortByBalance = (team1: Team, team2: Team) => {
-    return team1.account.balance - team2.account.balance;
-  }
-
-  // const teamsSortedBalance: ComputedRef<Team[]> = computed(() => {
-  //   const teamsToSort = structuredClone(toRaw(teams.value));
-  //   teamsToSort.sort(sortByBalance);
-  //   return teamsToSort;
-  // });
+    }).catch(() => {
+      teamsLoading.value = null;
+    })
+  })
 </script>
 
 <template>
-  
+  <div class="feed-container my-5 mx-auto">
+    <Loader v-if="tournamentsLoading === true" size="60px" background-color="#000000"/>
+    <div v-else-if="tournamentsLoading === null" class="alert alert-warning">
+      Failed to load tournaments, please try again.
+    </div>
+    <TournamentStatisticsCard v-else-if="!tournamentsLoading && tournaments !== null" v-for="tournament in tournaments" v-bind:tournament="tournament" v-bind:key="tournament.id" />
+  </div>
 </template>
