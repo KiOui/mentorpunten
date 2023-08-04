0ab3d2d382e82c5366623a7ff5bf78b831feb3aa
nvoers <nick-vanoers@hotmail.com>
2023-08-04 15:07:08 +0200

    header and fonts

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,10 +1,12 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
 import Footer from "@/components/Footer.vue";
+import Header from "@/components/Header.vue";
 </script>
 
 <template>
   <RouterView />
+  <Header/>
   <Footer/>
 </template>
 
--- frontend/src/components/Header.vue
+++ frontend/src/components/Header.vue
@@ -7,13 +7,20 @@ defineProps({
 </script>
 
 <template>
-  
+  <div class="header text-center py-2">
+    <h1>MENTORPUNTEN</h1>
+  </div>
 </template>
 
 <style scoped>
   .header {
     background-color: var(--nav-background-color);
     color: var(--nav-link-color);
+  }
+
+  .header h1{
+    font-family: 'Gill sans MT condensed', sans-serif;
+    margin-bottom: 0;
   }
 
   .information-screen {
