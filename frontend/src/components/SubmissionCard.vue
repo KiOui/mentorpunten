a0f6b6b98632a18a479186556abcddf05fa8d582
nvoers <nick-vanoers@hotmail.com>
2023-08-04 20:37:15 +0200

    changes feedview

--- frontend/src/components/SubmissionCard.vue
+++ frontend/src/components/SubmissionCard.vue
@@ -11,15 +11,19 @@ defineProps<{submission: Submission, showAccepted: boolean}>();
       <div class="d-flex flex-column h-100">
         <div class="row">
           <div class="col-8 submission-heading">
-              <h3>{{ submission.team.name }}</h3>
-              <h4>{{ submission.challenge.name }} - {{ submission.challenge.tournament.name }}</h4>
+              <router-link :to="{ name: 'Team', params: { id: submission.team.id } }" style="text-decoration: none;">
+                <h3>{{ submission.team.name }}</h3>
+              </router-link>
+              <router-link :to="{ name: 'Challenge', params: { id: submission.challenge.id } }" style="text-decoration: none;">
+                <h4>{{ submission.challenge.name }} - {{ submission.challenge.tournament.name }}</h4>
+              </router-link>
               <template v-if="showAccepted">
                 <div v-if="submission.accepted" class="badge bg-success">Accepted</div>
                 <div v-else-if="submission.accepted === false" class="badge bg-danger">Denied</div>
                 <div v-else class="badge bg-warning">Not processed yet</div>
               </template>
           </div>
-          <div class="col-4 text-end">
+          <div class="col-4 text-end my-auto" style="padding-right: 1.5rem;">
             <span v-if="submission.transaction !== null && submission.transaction.amount < 0" class="badge bg-danger fs-6">- {{ submission.transaction.amount * -1 }} points</span>
             <span v-if="submission.transaction !== null && submission.transaction.amount > 0" class="badge bg-success fs-6">+ {{ submission.transaction.amount }} points</span>
           </div>
