a5185a87920e2b2d89d7afb941a406e2d4078e85
Lars van Rhijn <larsvanrhijn@gmail.com>
2023-08-06 10:07:03 +0200

    Logout URL

--- backend/website/mentorpunten/urls.py
+++ backend/website/mentorpunten/urls.py
@@ -4,12 +4,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import RedirectView
 
-from .views import (
-    LogoutView,
-)
-
 urlpatterns = [
-    path("logout/", LogoutView.as_view(), name="logout"),
     path("thalia/", include(("thalia.urls", "thalia"), namespace="thalia")),
     path("api/", include("mentorpunten.api.urls")),
     path("oauth/", include("oauth2_provider.urls", namespace="oauth2_provider")),
@@ -25,11 +20,6 @@ urlpatterns = [
         "admin/login/",
         RedirectView.as_view(url="/login", query_string=True),
         name="login-redirect",
-    ),
-    path(
-        "admin/logout/",
-        RedirectView.as_view(url="/logout", query_string=True),
-        name="logout-redirect",
     ),
     path("admin-login/", admin.site.login, name="admin-login"),
     path("admin-logout/", admin.site.logout, name="admin-logout"),
