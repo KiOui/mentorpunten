e8883936643de07b4e73eeac8cb0b1e45af0d7e4
Lars van Rhijn <larsvanrhijn@gmail.com>
2023-08-06 18:24:27 +0200

    Styling changes

--- backend/website/users/views.py
+++ backend/website/users/views.py
@@ -4,19 +4,27 @@ from django.shortcuts import render, redirect
 from django.views.generic import TemplateView
 
 
-class LogoutView(LoginRequiredMixin, TemplateView):
+class LogoutView(TemplateView):
     """Logout View."""
 
     template_name = "users/logout.html"
 
     def get(self, request, **kwargs):
         """GET request."""
+        next_url = request.GET.get("next", "/")
+        if not request.user.is_authenticated:
+            return redirect(next_url)
+
         back_url = request.GET.get("back", "/")
         next_url = request.GET.get("next", "/")
         return render(request, self.template_name, {"next": next_url, "back": back_url})
 
     def post(self, request, **kwargs):
         """POST request."""
+        next_url = request.POST.get("next", "/")
+        if not request.user.is_authenticated:
+            return redirect(next_url)
+
         submit_value = request.POST.get("submit", None)
         if submit_value == 'Yes':
             logout(request)
