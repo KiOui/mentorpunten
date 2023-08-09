938b89152fe21adb6468df722b18a82c42dbe053
nvoers <nick-vanoers@hotmail.com>
2023-08-09 13:43:49 +0200

    display upcoming challenges

--- frontend/src/components/TournamentCard.vue
+++ frontend/src/components/TournamentCard.vue
@@ -3,12 +3,19 @@
     import type Tournament from "@/models/tournament.model";
     import {startEndTimeOfTournament} from "@/common/general.service";
 
-    defineProps<{tournament: Tournament}>();
+    const props = defineProps<{tournament: Tournament}>();
+
+    let current_time = Date.parse(new Date().toISOString());
     
 </script>
 
 <template>
-    <router-link :to="{ name: 'Challenges', params: { id: tournament.id }}" style="text-decoration: none;">
+    <div v-if="Date.parse(tournament.active_from) > current_time" class="custom-card disabled">
+        <h1>{{ tournament.name }}</h1>
+        <h3>{{ startEndTimeOfTournament(tournament) }}</h3>
+    </div>
+
+    <router-link v-else :to="{ name: 'Challenges', params: { id: tournament.id }}" style="text-decoration: none;">
         <div class="custom-card">
             <h1>{{ tournament.name }}</h1>
             <h3>{{ startEndTimeOfTournament(tournament) }}</h3>
