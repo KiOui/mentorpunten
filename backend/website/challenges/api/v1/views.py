7cc05de5aef54e13501f1552eefa688261634825
nvoers <nick-vanoers@hotmail.com>
2023-08-10 11:55:33 +0200

    not display disabled challenges

--- backend/website/challenges/api/v1/views.py
+++ backend/website/challenges/api/v1/views.py
@@ -25,7 +25,7 @@ class ChallengeListAPIView(ListAPIView):
 
     def get_queryset(self):
         """Get queryset."""
-        return models.Challenge.objects.all()
+        return models.Challenge.objects.filter(disabled=False)
 
 
 class ChallengeRetrieveAPIView(RetrieveAPIView):
