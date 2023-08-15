b2c8abaa8fc771d21c72f5e768fe21e37b56a599
Lars van Rhijn <larsvanrhijn@gmail.com>
2023-08-15 16:28:36 +0200

    Ordering different

--- frontend/src/views/ChallengesView.vue
+++ frontend/src/views/ChallengesView.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
   import ChallengeCard from "@/components/ChallengeCard.vue";
   import useApiService from "@/common/api.service";
-  import {onMounted, ref} from 'vue';
+  import {computed, onMounted, ref} from 'vue';
   import type Challenge from "@/models/challenge.model";
   import Loader from "@/components/Loader.vue";
   import Header from "@/components/Header.vue";
@@ -13,17 +13,75 @@
   const challengesLoading = ref<boolean | null>(true);
 
   onMounted(() => {
-    ApiService.getChallenges(new URLSearchParams([["tournament", String(props.id)], ["ordering", "active_from,active_until,name"]])).then(result => {
-      challenges.value = result.filter(function(challenge) {
-      if (challenge.active_until !== null){
-        return !(Date.parse(String(challenge.active_until)) < Date.parse(new Date().toISOString()));
-      } else {
-        return true;
-      }
-    });
+    ApiService.getChallenges(new URLSearchParams([["tournament", String(props.id)], ["ordering", "active,active_from,active_until,name"]])).then(result => {
+      challenges.value = result;
       challengesLoading.value = false;
     }).catch(() => {
       challengesLoading.value = null;
+    });
+  });
+
+  function challengeHasPassed(challenge: Challenge) {
+    if (challenge.active_until === null) {
+      return false;
+    }
+
+    return Date.parse(challenge.active_until) < Date.parse(new Date().toISOString());
+  }
+
+  function challengeStartsInFuture(challenge: Challenge) {
+    if (challenge.active_from === null) {
+      return false;
+    }
+
+    return Date.parse(challenge.active_from) > Date.parse(new Date().toISOString())
+  }
+
+  function challengeIsActive(challenge: Challenge) {
+    return !challengeHasPassed(challenge) && !challengeStartsInFuture(challenge);
+  }
+
+  function challengeIsNotActive(challenge: Challenge) {
+    return challengeHasPassed(challenge) || challengeStartsInFuture(challenge);
+  }
+
+  const challengesActive = computed(() => {
+    if (challenges.value === null) {
+      return null;
+    }
+
+    return challenges.value.filter((challenge) => {
+      return challengeIsActive(challenge);
+    });
+  });
+
+  const challengesLockedInThePast = computed(() => {
+    if (challenges.value === null) {
+      return null;
+    }
+
+    return challenges.value.filter((challenge) => {
+      return !challenge.completed && challengeHasPassed(challenge);
+    });
+  });
+
+  const challengesLockedInTheFuture = computed(() => {
+    if (challenges.value === null) {
+      return null;
+    }
+
+    return challenges.value.filter((challenge) => {
+      return !challenge.completed && challengeStartsInFuture(challenge);
+    });
+  });
+
+  const challengesDoneAndLocked = computed(() => {
+    if (challenges.value === null) {
+      return null;
+    }
+
+    return challenges.value.filter((challenge) => {
+      return challenge.completed && challengeIsNotActive(challenge);
     });
   });
 </script>
@@ -38,7 +96,12 @@
     <div v-else-if="challenges?.length === 0" class="alert alert-warning mx-1">
       There are no challenges for this tournament yet.
     </div>
-    <ChallengeCard v-else v-for="challenge in challenges" v-bind:challenge="challenge" v-bind:key="challenge.id" />
+    <template v-else>
+      <ChallengeCard v-for="challenge in challengesActive" v-bind:challenge="challenge" v-bind:key="challenge.id" />
+      <ChallengeCard v-for="challenge in challengesLockedInTheFuture" v-bind:challenge="challenge" v-bind:key="challenge.id" />
+      <ChallengeCard v-for="challenge in challengesDoneAndLocked" v-bind:challenge="challenge" v-bind:key="challenge.id" />
+      <ChallengeCard v-for="challenge in challengesLockedInThePast" v-bind:challenge="challenge" v-bind:key="challenge.id" />
+    </template>
   </div>
 </template>
 
