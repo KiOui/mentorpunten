9acccff793a72b92190fb8378a4a49a3489b5788
Lars van Rhijn <38525109+KiOui@users.noreply.github.com>
2023-08-24 00:55:43 +0200

    Don't show challenges with non revealed tournaments (#122)
    * Don't show challenges with non revealed tournaments
    
    * Add revealed to tournaments

--- backend/website/challenges/models.py
+++ backend/website/challenges/models.py
@@ -134,13 +134,13 @@ class Challenge(models.Model):
         if self.disabled:
             return False
 
+        if not self.tournament.revealed:
+            return False
+
         timezone = pytz.timezone(settings.TIME_ZONE)
         current_time = timezone.localize(datetime.now())
 
-        if self.active_from is not None and self.active_from > current_time:
-            return False
-        else:
-            return True
+        return self.active_from is None or self.active_from <= current_time
 
     def __str__(self):
         """Convert this object to string."""
